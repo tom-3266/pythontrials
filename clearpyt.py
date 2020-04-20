bccdef0d9ff2cc664351aeb232cd791d90e07ec9
tom sibi mattathil <44325772+tom-3266@users.noreply.github.com>
2020-04-20 11:40:51 +0530

    Add files via upload

---
+++ clearpyt.py
@@ -0,0 +1,13 @@
+from os import system, name 
+from time import sleep 
+def clear(): 
+  
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    else: 
+        _ = system('clear') 
+
+sleep(2) 
+
+clear()
