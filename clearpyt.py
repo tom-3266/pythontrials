4873d8d1f17720c4ad1097abbb9e474870e5085e
tom sibi mattathil <44325772+tom-3266@users.noreply.github.com>
2020-04-20 11:41:39 +0530

    Update clearpyt.py

--- clearpyt.py
+++ clearpyt.py
@@ -1,13 +1,12 @@
 from os import system, name 
 from time import sleep 
 def clear(): 
-  
     if name == 'nt': 
-        _ = system('cls') 
-  
+        _ = system('cls')  
     else: 
         _ = system('clear') 
 
 sleep(2) 
+
 
 clear()
