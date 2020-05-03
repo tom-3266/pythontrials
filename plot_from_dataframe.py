3d68db65e623049e48675fbbfccb81189144664d
tom sibi mattathil <44325772+tom-3266@users.noreply.github.com>
2020-05-03 13:36:11 +0530

    Update plot_from_dataframe.py

--- plot_from_dataframe.py
+++ plot_from_dataframe.py
@@ -7,3 +7,5 @@ car_sales["sale_date"]=pd.date_range('1/1/2020',periods=len(car_sales))
 car_sales["total_sale"]=car_sales["Price"].cumsum()
 car_sales.plot(x="sale_date",y="total_sale")
 plt.show()
+car_sales.plot(x="Odometer (KM)",y="total_sale",figsize=(8,5),kind="scatter");
+plt.show()
