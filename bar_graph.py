f3b87f7def20ee96cfdd25a8f5a3f06710f700a9
tom sibi mattathil <44325772+tom-3266@users.noreply.github.com>
2020-05-03 14:05:40 +0530

    Update bar_graph.py

--- bar_graph.py
+++ bar_graph.py
@@ -5,3 +5,14 @@ x = np.random.rand(10,4)
 df = pd.DataFrame(x,columns=['a','b','c','d'])
 df.plot.bar()
 plt.show()
+
+
+#or try this 
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+x = np.random.rand(10,4)
+df = pd.DataFrame(x,columns=['a','b','c','d'])
+df.plot(kind="bar",figsize=(10,5))
+plt.show()
